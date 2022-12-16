bd19a2584dd1cd553b7bbc363ff3af6125fac188
Sleiman Ghanem <you@example.com>
2022-12-16 23:32:22 +0200

    fixed error in pull request

---
+++ CSC_430_job_posting_website-frontend/js/post_job.js
@@ -0,0 +1,24 @@
+// Get function
+window.onload = () => {
+  // retrieve form element
+  const form = document.getElementById("form");
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    // create form data class and add data
+    const formData = new FormData(form);
+    console.log(...formData);
+
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/api/post",
+        formData
+      );
+      console.log(res);
+      window.location.href = "personal.html"
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  });
+};
---
+++ backend/app/Http/Controllers/JobController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class JobController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function post(Request $request)
+    {
+        // The user is logged in...
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255'
+        ]);
+        if (isset($request->skills)) {
+            $tags = $request->skills;
+        }
+        if (isset($request->skills1) && isset($tags)) {
+            $tags = $tags . ',' . $request->skills1;
+        } else if (isset($request->skills1)) {
+            $tags = $request->skills1;
+        }
+        if (isset($request->skills2) && isset($tags)) {
+            $tags = $tags . ',' . $request->skills2;
+        } else if (isset($request->skills2)) {
+            $tags = $request->skills2;
+        }
+
+        $user = Job::create([
+            'title' => $request->title,
+            'tags' => $tags,
+            'email' => $request->email,
+            'description' => $request->description,
+
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Job posted successfully'
+        ]);
+    }
+    public function get_jobs(){
+        $jobs = Job::all();
+        return json_encode($jobs);
+    }
+}
