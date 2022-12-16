dd7eb3d0b76f70a7b8b3e2bbd2f3eac3c91fb486
Sleiman Ghanem <you@example.com>
2022-12-16 16:13:43 +0200

    backend - finished initial user registration

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -46,14 +46,17 @@ class AuthController extends Controller
     public function register(Request $request){
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'email' => 'required|email|max:255'|'unique:users',
+            'password' => 'required|string',
+            'description' => 'required|string|max:255'
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'description' => $request->description,
+            'tags' => "",
         ]);
 
         $token = Auth::login($user);
